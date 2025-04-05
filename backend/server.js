fa5713265413b6937e5026950316e0dd5163e5c1
perpy <obasekiperpetua@gmail.com>
2025-04-05 22:45:36 +0100

    No changes was made

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 
 const __dirname = path.resolve();
 
