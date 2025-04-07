884fcf28147a0d6c52e4ea3851de8365fc37b500
perpy <obasekiperpetua@gmail.com>
2025-04-07 22:48:43 +0100

    updated the endpoint

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,13 @@ app.use(express.json()); // allows us to accept JSON data in the req.body
 app.use("/api/products", productRoutes);
 
 if(process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	
+}
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
-}
 
 
 app.listen(PORT, () => {
